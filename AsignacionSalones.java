c0bd03056eb05ebd7fc2c11f67848432c96ac3a1
Nery2004 <123770230+Nery2004@users.noreply.github.com>
2023-09-22 23:59:34 -0600

    Add files via upload

--- AsignacionSalones.java
+++ AsignacionSalones.java
@@ -1,16 +1,36 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.io.FileWriter; 
-import java.io.IOException; 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class AsignacionSalones {
     private List<Sede> sedes;
     private List<Curso> cursos;
+    private List<Curso> cursosNoAsignados;
 
     public AsignacionSalones() {
         this.sedes = new ArrayList<>();
         this.cursos = new ArrayList<>();
+        this.cursosNoAsignados = new ArrayList<>();
+    }
+    public Sede buscarSede(int idSede) {
+        for (Sede sede : sedes) {
+            if (sede.getId() == idSede) {
+                return sede;
+            }
+        }
+        return null;
+    }
+
+    public Edificio buscarEdificio(Sede sede, char edificio) {
+        for (Edificio edif : sede.getEdificios()) {
+            if (edif.getNombre().charAt(0) == edificio) {
+                return edif;
+            }
+        }
+        return null;
     }
 
     public static void main(String[] args) {
@@ -30,7 +50,7 @@ public class AsignacionSalones {
 
             System.out.print("Seleccione una opción: ");
             int opcion = scanner.nextInt();
-            scanner.nextLine(); // Consumir la nueva línea
+            scanner.nextLine();
 
             switch (opcion) {
                 case 1:
@@ -60,95 +80,90 @@ public class AsignacionSalones {
         System.out.println("¡Gracias por utilizar el programa!");
     }
 
-public void cargarSalonesDesdeTeclado(Scanner scanner) {
-    System.out.println("Ingrese los datos del salón:");
-    
-    // Solicitar datos al usuario
-    System.out.print("ID de sede: ");
-    int idSede = scanner.nextInt();
-    scanner.nextLine(); // Consumir la nueva línea
-    
-    System.out.print("Edificio: ");
-    char edificio = scanner.nextLine().charAt(0);
-    
-    System.out.print("Nivel: ");
-    int nivel = scanner.nextInt();
-    
-    System.out.print("ID de salón: ");
-    int idSalon = scanner.nextInt();
-    
-    System.out.print("Capacidad: ");
-    int capacidad = scanner.nextInt();
-    
-    // Crear una nueva sede si no existe
-    Sede sede = buscarSede(idSede);
-    if (sede == null) {
-        sede = new Sede(idSede);
-        sedes.add(sede);
-    }
-    
-    // Crear un nuevo edificio si no existe
-    Edificio edif = sede.buscarEdificio(edificio);
-    if (edif == null) {
-        edif = new Edificio(String.valueOf(edificio));
-        sede.agregarEdificio(edif);
-    }
-    
-    // Crear un nuevo salón y agregarlo al edificio
-    Salon salon = new Salon(idSalon, String.valueOf(edificio), capacidad);
-    edif.agregarSalon(salon);
-    
-    System.out.println("Salón agregado exitosamente.");
-}
+    public void cargarSalonesDesdeTeclado(Scanner scanner) {
+        System.out.println("Ingrese los datos del salón:");
 
-public void cargarCursosDesdeTeclado(Scanner scanner) {
-    System.out.println("Ingrese los datos del curso:");
-    
-    // Solicitar datos al usuario
-    System.out.print("ID del curso: ");
-    int idCurso = scanner.nextInt();
-    scanner.nextLine(); // Consumir la nueva línea
-    
-    System.out.print("ID de sede: ");
-    int idSede = scanner.nextInt();
-    scanner.nextLine(); // Consumir la nueva línea
-    
-    System.out.print("Nombre del curso: ");
-    String nombreCurso = scanner.nextLine();
-    
-    System.out.print("Horario (Ejemplo: 7:00 a.m. Lunes, Viernes): ");
-    String horario = scanner.nextLine();
-    
-    System.out.print("Duración (1 a 3 horas): ");
-    int duracion = scanner.nextInt();
-    scanner.nextLine(); // Consumir la nueva línea
-    
-    System.out.print("Días (Separados por comas, Ejemplo: lunes,viernes): ");
-    String dias = scanner.nextLine();
-    
-    System.out.print("Cantidad de estudiantes: ");
-    int cantidadEstudiantes = scanner.nextInt();
-    
-    // Crear un nuevo curso
-    Curso curso = new Curso(idCurso, idSede, nombreCurso, horario, duracion, dias, cantidadEstudiantes);
-    
-    // Agregar el curso a la lista de cursos
-    cursos.add(curso);
-    
-    System.out.println("Curso agregado exitosamente.");
-}
+        System.out.print("ID de sede: ");
+        int idSede = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Edificio: ");
+        char edificio = scanner.nextLine().charAt(0);
+
+        System.out.print("Nivel: ");
+        int nivel = scanner.nextInt();
+
+        System.out.print("ID de salón: ");
+        int idSalon = scanner.nextInt();
+
+        System.out.print("Capacidad: ");
+        int capacidad = scanner.nextInt();
+
+        Sede sede = buscarSede(idSede);
+        if (sede == null) {
+            sede = new Sede(idSede);
+            sedes.add(sede);
+        }
+
+        Edificio edif = buscarEdificio(sede, edificio);
+        if (edif == null) {
+            edif = new Edificio(String.valueOf(edificio));
+            sede.agregarEdificio(edif);
+        }
+
+        Salon salon = new Salon(idSalon, String.valueOf(edificio), capacidad);
+        edif.agregarSalon(salon);
+
+        System.out.println("Salón agregado exitosamente.");
+    }
+
+    public void cargarCursosDesdeTeclado(Scanner scanner) {
+        System.out.println("Ingrese los datos del curso:");
+
+        System.out.print("ID del curso: ");
+        int idCurso = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("ID de sede: ");
+        int idSede = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Nombre del curso: ");
+        String nombreCurso = scanner.nextLine();
+
+        System.out.print("Horario (Ejemplo: 7:00 a.m. Lunes, Viernes): ");
+        String horario = scanner.nextLine();
+
+        System.out.print("Duración (1 a 3 horas): ");
+        int duracion = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Días (Separados por comas, Ejemplo: lunes,viernes): ");
+        String diasString = scanner.nextLine();
+        List<String> dias = Arrays.asList(diasString.split(","));
+
+        System.out.print("Cantidad de estudiantes: ");
+        int cantidadEstudiantes = scanner.nextInt();
+
+        Curso curso = new Curso(idCurso, idSede, nombreCurso, horario, duracion, dias, cantidadEstudiantes);
+        cursos.add(curso);
+
+        System.out.println("Curso agregado exitosamente.");
+    }
+
 public void asignarSalones() {
-    // Implementa la lógica de asignación de salones a cursos aquí
-    for (Curso curso : cursos) {
-        boolean asignado = false;
-        for (Sede sede : sedes) {
-            for (Edificio edificio : sede.getEdificios()) {
-                for (Salon salon : edificio.getSalones()) {
-                    // Verificar si el salón es válido para el curso
-                    if (salon.puedeAsignarCurso(curso)) {
-                        // Asignar el curso al salón
-                        salon.asignarCurso(curso);
-                        asignado = true;
+        for (Curso curso : cursos) {
+            boolean asignado = false;
+            for (Sede sede : sedes) {
+                for (Edificio edificio : sede.getEdificios()) {
+                    for (Salon salon : edificio.getSalones()) {
+                        if (salon.puedeAsignarCurso(curso)) {
+                            salon.asignarCurso(curso);
+                            asignado = true;
+                            break;
+                        }
+                    }
+                    if (asignado) {
                         break;
                     }
                 }
@@ -156,83 +171,97 @@ public void asignarSalones() {
                     break;
                 }
             }
-            if (asignado) {
-                break;
+            if (!asignado) {
+                cursosNoAsignados.add(curso);
             }
         }
-        if (!asignado) {
-            cursosNoAsignados.add(curso);
-        }
+
+        System.out.println("Asignación de salones completada.");
     }
-    
-    System.out.println("Asignación de salones completada.");
-}
-public void generarInforme() {
-    System.out.println("Informe de Asignación de Salones:");
-    
-    // Mostrar cursos asignados
-    System.out.println("Cursos asignados a salones:");
-    for (Sede sede : sedes) {
-        for (Edificio edificio : sede.getEdificios()) {
-            for (Salon salon : edificio.getSalones()) {
-                if (!salon.getCursosAsignados().isEmpty()) {
-                    System.out.println("Sede: " + sede.getIdSede());
-                    System.out.println("Edificio: " + edificio.getNombre());
-                    System.out.println("Salón: " + salon.getIdSalon());
-                    System.out.println("Horario:");
-                    // Implementa la lógica para mostrar el horario del salón
-                    System.out.println("Cursos:");
-                    for (Curso curso : salon.getCursosAsignados()) {
-                        System.out.println("ID: " + curso.getIdCurso());
-                        System.out.println("Nombre: " + curso.getNombreCurso());
-                        System.out.println("Horario: " + curso.getHorario());
-                        System.out.println("Cantidad de Estudiantes: " + curso.getCantidadEstudiantes());
-                    }
-                    System.out.println();
-                }
-            }
-        }
-    }
-    
-    // Mostrar cursos no asignados
-    System.out.println("Cursos no asignados:");
-    for (Curso curso : cursosNoAsignados) {
-        System.out.println("ID: " + curso.getIdCurso());
-        System.out.println("Nombre: " + curso.getNombreCurso());
-        System.out.println("Horario: " + curso.getHorario());
-        System.out.println("Cantidad de Estudiantes: " + curso.getCantidadEstudiantes());
-    }
-}
-public void exportarResultados() {
-    try {
-        FileWriter csvWriter = new FileWriter("resultados.csv");
-        
-        // Escribir encabezado del archivo CSV
-        csvWriter.append("Sede,Edificio,Salon,Horario,Curso,Estudiantes\n");
-        
-        // Escribir datos de cursos asignados
+
+    public void generarInforme() {
+        System.out.println("Informe de Asignación de Salones:");
+
+        System.out.println("Cursos asignados a salones:");
         for (Sede sede : sedes) {
             for (Edificio edificio : sede.getEdificios()) {
                 for (Salon salon : edificio.getSalones()) {
-                    for (Curso curso : salon.getCursosAsignados()) {
-                        // Formatear los datos como una fila CSV
-                        String filaCSV = sede.getIdSede() + "," + edificio.getNombre() + "," +
-                                         salon.getIdSalon() + "," + curso.getHorario() + "," +
-                                         curso.getNombreCurso() + "," + curso.getCantidadEstudiantes() + "\n";
-                        
-                        csvWriter.append(filaCSV);
+                    if (!salon.getCursosAsignados().isEmpty()) {
+                        System.out.println("Sede: " + sede.getId());
+                        System.out.println("Edificio: " + edificio.getNombre());
+                        System.out.println("Salón: " + salon.getId());
+                        System.out.println("Horario:");
+                        for (Curso curso : salon.getCursosAsignados()) {
+                            System.out.println("ID: " + curso.getId());
+                            System.out.println("Nombre: " + curso.getNombre());
+                            System.out.println("Horario: " + curso.getHorario());
+                            System.out.println("Cantidad de Estudiantes: " + curso.getCantidadEstudiantes());
+                        }
+                        System.out.println();
                     }
                 }
             }
         }
-        
-        csvWriter.flush();
-        csvWriter.close();
-        
-        System.out.println("Resultados exportados exitosamente a resultados.csv");
-    } catch (IOException e) {
-        System.err.println("Error al exportar resultados: " + e.getMessage());
-    }
-}
 
+        System.out.println("Cursos no asignados:");
+        for (Curso curso : cursosNoAsignados) {
+            System.out.println("ID: " + curso.getId());
+            System.out.println("Nombre: " + curso.getNombre());
+            System.out.println("Horario: " + curso.getHorario());
+            System.out.println("Cantidad de Estudiantes: " + curso.getCantidadEstudiantes());
+            System.out.println();
+        }
+    }
+public void exportarResultados() {
+        try {
+            FileWriter cursosCsvWriter = new FileWriter("cursos.csv");
+            FileWriter salonesCsvWriter = new FileWriter("salones.csv");
+
+            // Escribir encabezado del archivo CSV de cursos
+            cursosCsvWriter.append("Sede,Edificio,Salon,Horario,Curso,Estudiantes\n");
+
+            // Escribir encabezado del archivo CSV de salones
+            salonesCsvWriter.append("ID,Edificio,Nivel,Salon,Capacidad\n");
+
+            // Escribir datos de cursos asignados
+            for (Sede sede : sedes) {
+                for (Edificio edificio : sede.getEdificios()) {
+                    for (Salon salon : edificio.getSalones()) {
+                        for (Curso curso : salon.getCursosAsignados()) {
+                            // Formatear los datos como una fila CSV para cursos
+                            String filaCursoCSV = sede.getId() + "," + edificio.getNombre() + "," +
+                                                 salon.getIdSalon() + "," + curso.getHorario() + "," +
+                                                 curso.getNombreCurso() + "," + curso.getCantidadEstudiantes() + "\n";
+
+                            cursosCsvWriter.append(filaCursoCSV);
+                        }
+                    }
+                }
+            }
+
+            // Escribir datos de los salones
+            for (Sede sede : sedes) {
+                for (Edificio edificio : sede.getEdificios()) {
+                    for (Salon salon : edificio.getSalones()) {
+                        // Formatear los datos como una fila CSV para salones
+                        String filaSalonCSV = salon.getIdSalon() + "," + edificio.getNombre() + "," +
+                                             salon.getNivel() + "," + salon.getNombreSalon() + "," +
+                                             salon.getCapacidad() + "\n";
+
+                        salonesCsvWriter.append(filaSalonCSV);
+                    }
+                }
+            }
+
+            cursosCsvWriter.flush();
+            cursosCsvWriter.close();
+
+            salonesCsvWriter.flush();
+            salonesCsvWriter.close();
+
+            System.out.println("Archivos CSV 'cursos.csv' y 'salones.csv' exportados exitosamente.");
+        } catch (IOException e) {
+            System.err.println("Error al exportar resultados: " + e.getMessage());
+        }
+}
 }
--- Curso.java
+++ Curso.java
@@ -21,48 +21,24 @@ public class Curso {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getNombre() {
         return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
     }
 
     public String getHorario() {
         return horario;
     }
 
-    public void setHorario(String horario) {
-        this.horario = horario;
-    }
-
     public int getDuracion() {
         return duracion;
-    }
-
-    public void setDuracion(int duracion) {
-        this.duracion = duracion;
     }
 
     public List<String> getDias() {
         return dias;
     }
 
-    public void setDias(List<String> dias) {
-        this.dias = dias;
-    }
-
     public int getCantidadEstudiantes() {
         return cantidadEstudiantes;
-    }
-
-    public void setCantidadEstudiantes(int cantidadEstudiantes) {
-        this.cantidadEstudiantes = cantidadEstudiantes;
     }
 
     @Override
